3fc35926ffc6d173a50e19c4e9206c7e43ad7054
Gabriele DAmelio Sanches <gabriele.dameliosanches49@baruchmail.cuny.edu>
2024-04-04 21:12:02 -0400

    -

--- scripts/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-config_file_path = 'config/config.json'
-
-#load the JSON configuration file
-with open(config_file_path, 'r') as config_file:
-    config = json.load(config_file)
-
-Connection_STRING = config["connectionString"]
